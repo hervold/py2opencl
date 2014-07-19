b08a8ef60f052b96c42dfff308f07c0970b23b15
kieran hervold <hervold@gmail.com>
2014-07-18 22:35:23 -0700

    .

--- README.txt
+++ README.txt
@@ -10,6 +10,6 @@ kernel (written in a C-like language), then uses Andreas Klöckner's PyOpenCL
 module to submit the kernel to the GPU.  It is not meant to convert arbitrary
 Python code to OpenCL, as that would be impossible.  Instead, it is limited to
 Python lambdas containing only simple mathematical operations and built-in
-OpenCL functions are supported.
+OpenCL functions.
 
 
--- py2opencl/test/test.py
+++ py2opencl/test/test.py
@@ -4,8 +4,8 @@ simple test of py2opencl
 
 import numpy as np
 import time
-from .driver import Py2OpenCL
-from . import F
+from ..driver import Py2OpenCL
+from .. import F
 
 
 def main():
@@ -21,7 +21,7 @@ def main():
     before = time.time()
     b = np.where( arr < 0.5, -arr, np.sin(arr) )
     print "sine - numpy: for %d elements, took" % len(a), time.time() - before
-    #print a - b
+    print "max delta: %.2e\n" % np.max(a - b)
 
     before = time.time()
     a = Py2OpenCL( lambda x: F.atanpi(x) ).map( arr )
@@ -29,7 +29,6 @@ def main():
     before = time.time()
     b = (lambda x: F.atanpi(x) / np.pi)( arr )
     print "arctan(x) / pi - numpy: for %d elements, took" % len(a), time.time() - before
-    return
 
     for n in (100, 10000, 1000000, 10000000):
         rnd = np.random.rand(n).astype(np.float32)
@@ -53,9 +52,8 @@ def main():
         before = time.time()
         r2 = a + b
         print "Simple sum - numpy (size=1e%s):" % math.log10(size), time.time() - before
-        print r2 - res
+        print "max delta: %.2e\n" % np.max(r2 - res)
 
-    print
     two = Py2OpenCL( lambda x, y: x * y )
     for size in (1e4, 1e5, 1e6, 1e7):
         a, b = np.random.rand(int(1e7)).astype(np.float32), np.random.rand(int(1e7)).astype(np.float32)
@@ -66,7 +64,7 @@ def main():
         before = time.time()
         r2 = a * b
         print "Simple multiplication - numpy (size=1e%s):" % int(math.log10(size)), time.time() - before
-        print r2 - res
+        print "max delta: %.2e\n" % np.max(r2 - res)
 
     print
     two = Py2OpenCL( lambda x, y: x ** y )
@@ -79,7 +77,7 @@ def main():
         before = time.time()
         r2 = a ** b
         print "Simple exponents - numpy (size=1e%s):" % int(math.log10(size)), time.time() - before
-        print r2 - res
+        print "max delta: %.2e\n" % np.max(r2 - res)
 
 
 if __name__ == '__main__':

b889f889c58a78f945b338aab94e5f66db3ae1e9
kieran hervold <hervold@gmail.com>
2014-07-26 23:07:39 -0700

    forgot the description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['py2opencl', 'py2opencl.test'],
     url='http://pypi.python.org/pypi/py2opencl/',
     license='LICENSE.txt',
-    description='Useful towel-related stuff.',
+    description='auto-creation of OpenCL kernels from pure Python code',
     long_description=open('README.txt').read(),
     install_requires=[
         "pyopencl >= 2014.1"

764b73afa44fd0b358627eaaa4f4e49e8de010ea
kieran hervold <hervold@gmail.com>
2014-07-26 23:08:33 -0700

    had to bump the version # to reflect my changed description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py2opencl',
-    version='0.1.0',
+    version='0.1.1',
     author='kieran hervold',
     author_email='hervold@gmail.com',
     packages=['py2opencl', 'py2opencl.test'],
